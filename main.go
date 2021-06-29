ce1c9c9d3ee1715da080be6ece61b673e1796a53
spicycurry <18017686817@163.com>
2021-06-29 23:15:21 +0800

    fix README.md

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-/*
-(1) global middleware Logger
-$ curl http://localhost:9999/
-<h1>Hello Gee</h1>
-
->>> log
-2019/08/17 01:37:38 [200] / in 3.14µs
-*/
-
-/*
-(2) global + group middleware
-$ curl http://localhost:9999/v2/hello/geektutu
-{"message":"Internal Server Error"}
-
->>> log
-2019/08/17 01:38:48 [200] /v2/hello/geektutu in 61.467µs for group v2
-2019/08/17 01:38:48 [200] /v2/hello/geektutu in 281µs
-*/
-
 import (
 	"gee"
 	"log"
